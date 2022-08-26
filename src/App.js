66e9a7bd674d6f8c40942706fc9fcfe97be6b425
lukwa999 <lukwa555@gmail.com>
2022-08-26 10:50:00 +0700

    add func-component.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Header,Content,Footer } from './component';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    [<Header/>,<Content/>,<Footer/>]
+  );
+}
+
+export default App;
